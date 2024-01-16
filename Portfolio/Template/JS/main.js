d600a30b1f1cd5e5e0867d6057e40b3655c5bd3a
JeremyMarkWilcox <jeremymarkwilcox@gmail.com>
2024-01-15 21:08:01 -0500

    Pushing changes and the template that I'm learning from in a folder.

--- Portfolio/Template/JS/main.js
+++ Portfolio/Template/JS/main.js
@@ -44,4 +44,4 @@ document.addEventListener("click", function(event){
     if (event.target.matches(".cancel") || !event.target.closest(".form-popup") && !event.target.closest(".Pop_Up_Button") && !event.target.closest(".contact")){
         closeForm()
     }
-}, false ) 
+}, false )
